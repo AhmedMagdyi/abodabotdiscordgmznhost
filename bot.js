e14307f2e90e43e6b348f22b0209d6611df6905d
AhmedMagdyi <43391576+AhmedMagdyi@users.noreply.github.com>
2018-10-18 14:33:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,5 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const fs = require("fs");
-const { Client, Util } = require('discord.js');
-const getYoutubeID = require('get-youtube-id');
-const fetchVideoInfo = require('youtube-info');
-const YouTube = require('simple-youtube-api');
-const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
-const queue = new Map();
-const UserBlocked = new Set();
 const prefix = '^'
  
 client.on('ready', () => {
